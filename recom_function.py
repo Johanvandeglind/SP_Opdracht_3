6f01f4d982c7172d4981bdb518a40156c1af1fa9
Johanvandeglind <31281194+Johanvandeglind@users.noreply.github.com>
2021-03-17 13:09:57 +0100

    Started with collaborative filtering

--- recom_function.py
+++ recom_function.py
@@ -12,7 +12,7 @@ con = psycopg2.connect(
 cur = con.cursor()
 
 
-def create_content_recommendation_table(table, colmn, value, order):
+def create_content_recommendation_table(colmn, value, order):
     sql = []
     cur.execute("""CREATE TABLE IF NOT EXISTS recommendation_%s 
                     (id VARCHAR PRIMARY KEY,
@@ -29,35 +29,65 @@ def create_content_recommendation_table(table, colmn, value, order):
                      stock INTEGER );""" % (value))
     con.commit()
     cur.execute("DELETE FROM recommendation_%s"% (value))
-    cur.execute(f"SELECT * FROM {table} WHERE ({colmn} = '{value}' and selling_price > 0 and stock_level > 0) ORDER BY {order} ASC LIMIT 4 ")  # % (table,colmn,value,))
+    cur.execute(f"SELECT * FROM product  WHERE ({colmn} = '{value}' and selling_price > 0 and stock_level > 0)  ORDER BY {order} ASC limit 4 ")  # % (table,colmn,value,))
     records = cur.fetchall()
+    #print(records)
     product = []
     for record in records:
         #print(record)
-        cur.execute(f"SELECT * FROM properties WHERE (productid = '{record[0]}') ORDER BY productid ASC")  # % (table,colmn,value,))
+        cur.execute(f"SELECT * FROM properties WHERE (productid = '{record[0]}') ORDER BY productid ASC")
         properties = cur.fetchall()
-        #print(properties)
-        deal =None
-        types = None
-        doelgroep = None
+        deal = None
+        types =  None
+        doelgroep =None
         for prop in properties:
-            #print(prop[1])
             if prop[1] == 'discount':
                 deal = prop[2]
             if prop[1] =='soort':
                 types = prop[2]
             if prop[1] == 'doelgroep':
                 doelgroep = prop[2]
-
+    #
         sql.append((record[0], record[1], record[3], types, record[8], record[9], record[10], doelgroep, record[2],deal, record[6],record[13]))
-        print(properties)
+    print(f"recommendation_{value} table created/filled")
     cur.executemany(f"INSERT INTO recommendation_{value} VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",sql)
     con.commit()
 
 
-create_content_recommendation_table('product', 'sub_sub_category', 'Deodorant', 'selling_price')
-create_content_recommendation_table('product','brand','Nivea','selling_price')
+create_content_recommendation_table('sub_sub_category', 'Deodorant', 'selling_price')
+#create_content_recommendation_table('brand','Nivea','selling_price')
 
+def create_collaborative_recommendation_table(profil_id, value):
+    sql = []
+    # cur.execute("""CREATE TABLE IF NOT EXISTS recommendation_%s
+    #                 (id VARCHAR PRIMARY KEY,
+    #                  name VARCHAR,
+    #                  brand VARCHAR,
+    #                  type VARCHAR,
+    #                  category VARCHAR,
+    #                  subcategory VARCHAR,
+    #                  subsubcategory VARCHAR,
+    #                  targetaudience VARCHAR,
+    #                  selling_price INTEGER,
+    #                  deal VARCHAR,
+    #                  description VARCHAR,
+    #                  stock INTEGER );""" % (value))
+    # con.commit()
+    # cur.execute("DELETE FROM recommendation_%s"% (value))
 
+    cur.execute(f"SELECT product_id FROM orders inner join buids on orders.sessionsid = buids.sessionsid where(buids.profileprofile_id = '{profil_id}') ")
+    #cur.execute(f"SELECT productid FROM viewed_before where(profileprofile_id = '{profil_id}') ")
+    records = cur.fetchall()
+
+    for record in records:
+        print (record)
+    print("--------------------------------------")
+    cur.execute(f"SELECT productid FROM viewed_before where(profileprofile_id = '{profil_id}') ")
+    records = cur.fetchall()
+
+    for record in records:
+        print(record)
+
+create_collaborative_recommendation_table('5a393d68ed295900010384ca','5a393d68ed295900010384ca')
 cur.close()
 con.close()
